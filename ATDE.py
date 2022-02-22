c7effd816618c1699cbf33a71a7ba42ba8da5721
Huy Tang <64514608+HuyMax@users.noreply.github.com>
2022-02-22 14:36:45 +0900

    Update ATDE.py

--- ATDE.py
+++ ATDE.py
@@ -4,10 +4,14 @@ import numpy.matlib
 import random
 import scipy.stats as stats
 
-# Notes:
-# *** -> Check when changing problems
+# This is the code of the algorithm in the paper: A novel adaptive 3-stage hybrid teaching-based differential evolution algorithm for frequency-constrained truss designs,
+# Structures (2022). Please feel free to use it with an appropriate citation.
+# Usage:
+# Evaluate_Objective: the objective function
+# with input: problem label, population size, a candidate solution vector, and penalty factors
+# and output: penalized objective function value of the candidate solution and its constraint violation.
 
-def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, threshold1, threshold2):
+def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penalty_factor, MaxIter, tol, threshold1, threshold2):
     Iter_Plot = np.zeros((MaxIter, 1), dtype=float)
     nFEAs_Plot = np.zeros((MaxIter, 1), dtype=float)
     fbest_Plot = np.zeros((MaxIter, 1), dtype=float)
@@ -17,14 +21,13 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
     x = np.matlib.repmat(LB, NP, 1) + np.random.uniform(0, 1, (NP, nDVs)) * np.matlib.repmat(UB - LB, NP, 1)
 
     # Evaluate the objective function w.r.t constraints
-    f, g = Evaluate_Objective(Label, NP, x, penal)
+    f, g = Evaluate_Objective(Label, NP, x, penalty_factor)
     nFEAs = 0 # number of finite element analyses (FEAs)
 
     sorted_ind = np.argsort(f[:, 0], axis=0)
 
     ibest = sorted_ind[0]
     ibest2 = sorted_ind[1]
-    ibest3 = sorted_ind[2]
     xbest = x[ibest,:]
     fbest = f[ibest,0]
     gbest = g[ibest,0]
@@ -40,7 +43,7 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
     step = (b - a) / (2*TC)
     for Iter in range(MaxIter):
         # *** Update penalty ***
-        penal[1] = min(3, penal[1] + 0.05)
+        penalty_factor[1] = min(3, penalty_factor[1] + 0.05)
 
         if Iter < TC:
             a, b = a+step, b-step
@@ -55,18 +58,18 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
                 fMean_of_3 = np.mean(fR)
                 R0 = np.argmin(fR, axis=0)
 
-                if f[i,0] < fMean_of_3: # i-th student is better than mean of 3 random students
-                    mean_of_3 = (x[R[0], :] + x[R[1], :] + x[R[2], :]) / 3  # mean student of 3 students
+                if f[i,0] < fMean_of_3: # condition for teaching based mutation
+                    mean_of_3 = (x[R[0], :] + x[R[1], :] + x[R[2], :]) / 3  # Eq. 13
                     TF = 1 + np.random.randint(2, size=(nDVs,))
-                    r = a + (b - a) * np.random.random(size=(nDVs,))
+                    r = a + (b - a) * np.random.random(size=(nDVs,))       # Eq. 15
                     if f[i,0] < fR[R0]:
-                        v_m = x[i, :] + r * (x[R[R0], :] - TF * mean_of_3)
+                        v_m = x[i, :] + r * (x[R[R0], :] - TF * mean_of_3) # Eq. 14a
                     else:
-                        v_m = x[R[R0], :] + r * (x[i, :] - TF * mean_of_3)
+                        v_m = x[R[R0], :] + r * (x[i, :] - TF * mean_of_3) # Eq. 14b
 
                 else:
                     F = a + (b - a) * np.random.random(size=(nDVs,))
-                    v_m = x[R[0], :] + F * (x[R[1], :] - x[R[2], :]) # rand/1
+                    v_m = x[R[0], :] + F * (x[R[1], :] - x[R[2], :])
 
             else:
                 candidates = list(range(0, NP))
@@ -80,11 +83,11 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
                 F = a + (b - a) * np.random.random(size=(nDVs,))
 
                 if delta <= threshold2:
-                    v_m = x[Rbest, :] + F * (x[R[0], :] - x[R[1], :])  # 2best/1
+                    v_m = x[Rbest, :] + F * (x[R[0], :] - x[R[1], :])  # Eq. 17
                 else:
                     if i == ibest: Rbest = ibest
                     else: Rbest = np.random.choice([ibest, ibest2])
-                    v_m = x[i, :] + F * (x[Rbest,:] - x[i, :]) + F * (x[R[0], :] - x[R[1], :])  # current-to-2best/1
+                    v_m = x[i, :] + F * (x[Rbest,:] - x[i, :]) + F * (x[R[0], :] - x[R[1], :])  # Eq. 16
 
             # Return design variables violated bounds to the search space
             v_m = (v_m >= LB)*v_m + (v_m < LB)*(2*LB - v_m)
@@ -102,7 +105,7 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
             u[i, :] = t*v[i, :] + (1-t)*x[i, :]
 
         # Phase 4: Selection
-        fnew, gnew = Evaluate_Objective(Label, NP, u, penal)
+        fnew, gnew = Evaluate_Objective(Label, NP, u, penalty_factor)
         nFEAs += NP
 
         # elitist
@@ -117,7 +120,6 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
         sorted_ind = np.argsort(f[:, 0], axis=0)
         ibest = sorted_ind[0]
         ibest2 = sorted_ind[1]
-        ibest3 = sorted_ind[2]
         xbest = xnew[ibest, :]
         fbest = f[ibest, 0]
         gbest = g[ibest, 0]
@@ -129,10 +131,10 @@ def ATDE(Evaluate_Objective, Label, LB, UB, nDVs, NP, penal, MaxIter, tol, thres
         fbest_Plot[Iter, 0] = fbest
         gbest_Plot[Iter, 0] = gbest
 
-        delta = abs(abs(fmean)/abs(fbest) - 1) # for truss
+        delta = abs(abs(fmean)/abs(fbest) - 1)
         print('Iter: ' + str(Iter) + ', nFEAs: ' + str(nFEAs) + ', fbest: ' + str(fbest) + ', fmean: ' + str(fmean))
 
-        if delta <= tol or Iter == MaxIter: # for truss
+        if delta <= tol or Iter == MaxIter: # stopping criteria
             break
 
     return (xbest, fbest, Iter, nFEAs, Iter_Plot[0:Iter+1, 0], nFEAs_Plot[0:Iter+1, 0], fbest_Plot[0:Iter+1, 0], gbest_Plot[0:Iter+1, 0])
